a6a97974c702dd4fc5c56bd9b27fd06e337da5e5
sebastianjkern <sebastianjakobkern@gmail.com>
2022-09-24 00:42:55 +0200

    Switch to different bezier sdf function

--- sdf_ui/shader/fragment.glsl
+++ sdf_ui/shader/fragment.glsl
@@ -51,66 +51,54 @@ float sdf_linesegment(in vec2 uv, in vec2 a, in vec2 b, in float radius)
 float dot2(in vec2 v) { return dot(v, v); }
 float cro(in vec2 a, in vec2 b) { return a.x*b.y - a.y*b.x; }
 
-float sdf_quadratic_bezier(in vec2 pos, in vec2 A, in vec2 B, in vec2 C)
+// Test if point p crosses line (a, b), returns sign of result
+float testCross(vec2 a, vec2 b, vec2 p) {
+    return sign((b.y-a.y) * (p.x-a.x) - (b.x-a.x) * (p.y-a.y));
+}
+
+// Determine which side we're on (using barycentric parameterization)
+float signBezier(vec2 A, vec2 B, vec2 C, vec2 p)
 {
-    vec2 a = B - A;
-    vec2 b = A - 2.0*B + C;
-    vec2 c = a * 2.0;
-    vec2 d = A - pos;
+    vec2 a = C - A, b = B - A, c = p - A;
+    vec2 bary = vec2(c.x*b.y-b.x*c.y, a.x*c.y-c.x*a.y) / (a.x*b.y-b.x*a.y);
+    vec2 d = vec2(bary.y * 0.5, 0.0) + 1.0 - bary.x - bary.y;
+    return mix(sign(d.x * d.x - d.y), mix(-1.0, 1.0,
+    step(testCross(A, B, p) * testCross(B, C, p), 0.0)),
+    step((d.x - d.y), 0.0)) * testCross(A, C, B);
+}
 
-    float kk = 1.0/dot(b, b);
-    float kx = kk * dot(a, b);
-    float ky = kk * (2.0*dot(a, a)+dot(d, b))/3.0;
-    float kz = kk * dot(d, a);
-
-    float res = 0.0;
-    float sgn = 0.0;
-
-    float p  = ky - kx*kx;
-    float q  = kx*(2.0*kx*kx - 3.0*ky) + kz;
-    float p3 = p*p*p;
-    float q2 = q*q;
-    float h  = q2 + 4.0*p3;
-
-    if (h>=0.0)
-    { // 1 root
-        h = sqrt(h);
-        vec2 x = (vec2(h, -h)-q)/2.0;
-
-        #if 0
-        // When p≈0 and p<0, h-q has catastrophic cancelation. So, we do
-        // h=√(q²+4p³)=q·√(1+4p³/q²)=q·√(1+w) instead. Now we approximate
-        // √ by a linear Taylor expansion into h≈q(1+½w) so that the q's
-        // cancel each other in h-q. Expanding and simplifying further we
-        // get x=vec2(p³/q,-p³/q-q). And using a second degree Taylor
-        // expansion instead: x=vec2(k,-k-q) with k=(1-p³/q²)·p³/q
-        if (abs(p)<0.001)
-        {
-            float k = p3/q;// linear approx
-            // float k = (1.0-p3/q2)*p3/q;  // quadratic approx
-            x = vec2(k, -k-q);
-        }
-            #endif
-
+// Solve cubic equation for roots
+vec3 solveCubic(float a, float b, float c)
+{
+    float p = b - a*a / 3.0, p3 = p*p*p;
+    float q = a * (2.0*a*a - 9.0*b) / 27.0 + c;
+    float d = q*q + 4.0*p3 / 27.0;
+    float offset = -a / 3.0;
+    if (d >= 0.0) {
+        float z = sqrt(d);
+        vec2 x = (vec2(z, -z) - q) / 2.0;
         vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
-        float t = clamp(uv.x+uv.y-kx, 0.0, 1.0);
-        vec2  q = d+(c+b*t)*t;
-        res = dot2(q);
-        sgn = cro(c+2.0*b*t, q);
+        return vec3(offset + uv.x + uv.y);
     }
-    else
-    { // 3 roots
-        float z = sqrt(-p);
-        float v = acos(q/(p*z*2.0))/3.0;
-        float m = cos(v);
-        float n = sin(v)*1.732050808;
-        vec3  t = clamp(vec3(m+m, -n-m, n-m)*z-kx, 0.0, 1.0);
-        vec2  qx=d+(c+b*t.x)*t.x; float dx=dot2(qx), sx = cro(c+2.0*b*t.x, qx);
-        vec2  qy=d+(c+b*t.y)*t.y; float dy=dot2(qy), sy = cro(c+2.0*b*t.y, qy);
-        if (dx<dy) { res=dx; sgn=sx; } else { res=dy; sgn=sy; }
-    }
+    float v = acos(-sqrt(-27.0 / p3) * q / 2.0) / 3.0;
+    float m = cos(v), n = sin(v)*1.732050808;
+    return vec3(m + m, -n - m, n - m) * sqrt(-p / 3.0) + offset;
+}
 
-    return sqrt(res)*sign(sgn) + radius;
+// Find the signed distance from a point to a bezier curve
+float sdf_quadratic_bezier(vec2 p, vec2 A, vec2 B, vec2 C)
+{
+    B = mix(B + vec2(1e-4), B, abs(sign(B * 2.0 - A - C)));
+    vec2 a = B - A, b = A - B * 2.0 + C, c = a * 2.0, d = A - p;
+    vec3 k = vec3(3.*dot(a, b), 2.*dot(a, a)+dot(d, b), dot(d, a)) / dot(b, b);
+    vec3 t = clamp(solveCubic(k.x, k.y, k.z), 0.0, 1.0);
+    vec2 pos = A + (c + b*t.x)*t.x;
+    float dis = length(pos - p);
+    pos = A + (c + b*t.y)*t.y;
+    dis = min(dis, length(pos - p));
+    pos = A + (c + b*t.z)*t.z;
+    dis = min(dis, length(pos - p));
+    return dis * signBezier(A, B, C, p);
 }
 
 vec3 rgb2xyz(vec3 c)
