876bde6f29621131bf7dfc05c89355c3fa5719d8
sebastianjkern <sebastianjakobkern@gmail.com>
2023-11-16 11:31:18 +0100

    Fix bug

--- text_rendering_sandbox.py
+++ text_rendering_sandbox.py
@@ -2,10 +2,9 @@
 
 import logging
 
-from framework.context import Context
-from framework.log import logger
-from framework.main import set_context, clear_color, line
-from framework.util import hex_col
+from framework import Context
+from framework import logger
+from framework import set_context, line
 
 logger().setLevel(logging.INFO)
 
@@ -19,6 +18,6 @@ l1 = line((250, 100), (50, 450))
 l2 = line((250, 100), (450, 450))
 l3 = line((100, 250), (400, 250))
 
-l1.union(l2).union(l3).fill(inflate=5, fg_color=(1,1,1,1), bg_color=(0,0,0,1)).to_rgb().show()
+l1.union(l2).union(l3).fill(inflate=5, fg_color=(1, 1, 1, 1), bg_color=(0, 0, 0, 1)).to_rgb().show()
 # .fill(inflate=5, fg_color=(1.0, 1.0, 1.0, 1.0), bg_color=(0,0,0,1)).to_rgb().show()
 # %%
